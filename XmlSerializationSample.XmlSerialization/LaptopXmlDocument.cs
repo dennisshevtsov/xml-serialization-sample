32a1d7b17544e9df17c6d7d661118307616d8805
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-04-08 17:52:49 +0300

    license

--- XmlSerializationSample.XmlSerialization/LaptopXmlDocument.cs
+++ XmlSerializationSample.XmlSerialization/LaptopXmlDocument.cs
@@ -1,7 +1,11 @@
-﻿using System.Xml.Serialization;
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
 
 namespace XmlSerializationSample.XmlSerialization
 {
+  using System.Xml.Serialization;
+
   public sealed class LaptopXmlDocument : ProductXmlDocumentBase
   {
     [XmlElement("screen")]
--- XmlSerializationSample.XmlSerialization/Serializer.cs
+++ XmlSerializationSample.XmlSerialization/Serializer.cs
@@ -1,4 +1,8 @@
-﻿namespace XmlSerializationSample.XmlSerialization
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace XmlSerializationSample.XmlSerialization
 {
   using System;
   using System.IO;
