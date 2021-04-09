d7c0c7863eb5b5d642e0cd1a25058ba558cd84f4
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-04-09 13:37:13 +0300

    start impl

---
+++ XmlSerializationSample.XmlSerialization/Serializer0.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace XmlSerializationSample.XmlSerialization
+{
+  using System;
+  using System.IO;
+  using System.Threading;
+  using System.Threading.Tasks;
+
+  using Microsoft.IO;
+
+  public sealed class Serializer0 : ISerializer
+  {
+    private readonly RecyclableMemoryStreamManager _recyclableMemoryStreamManager;
+
+    public Serializer0(RecyclableMemoryStreamManager recyclableMemoryStreamManager)
+    {
+      _recyclableMemoryStreamManager = recyclableMemoryStreamManager ?? throw new ArgumentNullException(nameof(recyclableMemoryStreamManager));
+    }
+
+    public Task<string> SerializeAsync(object document, CancellationToken cancellationToken)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task SerializeAsync(object document, Stream stream, CancellationToken cancellationToken)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<object> DeserializeAsync(string input, Type type, CancellationToken cancellationToken)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<object> DeserializeAsync(Stream input, Type type, CancellationToken cancellationToken)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<TDocument> DeserializeAsync<TDocument>(string input, CancellationToken cancellationToken)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<TDocument> DeserializeAsync<TDocument>(Stream input, CancellationToken cancellationToken)
+    {
+      throw new NotImplementedException();
+  }
+}
+}
