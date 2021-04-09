c0bb958c7b2377f52c9abcf443b791508c7822df
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-04-09 13:20:26 +0300

    interface

---
+++ XmlSerializationSample.XmlSerialization/ISerializer.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace XmlSerializationSample.XmlSerialization
+{
+  using System;
+  using System.IO;
+  using System.Threading;
+  using System.Threading.Tasks;
+
+  public interface ISerializer
+  {
+    public Task<string> SerializeAsync(object document, CancellationToken cancellationToken);
+
+    public Task SerializeAsync(object document, Stream stream, CancellationToken cancellationToken);
+
+    public Task<object> DeserializeAsync(string input, Type type, CancellationToken cancellationToken);
+
+    public Task<object> DeserializeAsync(Stream input, Type type, CancellationToken cancellationToken);
+
+    public Task<TDocument> DeserializeAsync<TDocument>(string input, CancellationToken cancellationToken);
+
+    public Task<TDocument> DeserializeAsync<TDocument>(Stream input, CancellationToken cancellationToken);
+  }
+}
