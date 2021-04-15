de2279d1ccd39ee5433243c175f212304ac4bc6a
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-04-16 01:28:04 +0300

    extensions

---
+++ XmlSerializationSample.XmlSerialization/ServicesExtensions.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace XmlSerializationSample.XmlSerialization
+{
+  using System;
+
+  using Microsoft.Extensions.DependencyInjection;
+
+  public static class ServicesExtensions
+  {
+    public static IServiceCollection AddSerialization(
+      this IServiceCollection services, Action<ISerializerConfig> configure)
+    {
+      if (services == null)
+      {
+        throw new ArgumentNullException(nameof(services));
+      }
+
+      if (configure == null)
+      {
+        throw new ArgumentNullException(nameof(configure));
+      }
+
+      var provider = new SerializerProvider();
+
+      configure.Invoke(provider);
+
+      services.AddSingleton(provider);
+
+      return services;
+    }
+  }
+}
