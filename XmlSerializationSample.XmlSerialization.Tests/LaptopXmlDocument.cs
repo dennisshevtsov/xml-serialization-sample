5cb2a4ba2dc9453663fe125d948f2da0681010ff
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-04-17 00:23:06 +0300

    move xml docs

--- XmlSerializationSample.XmlSerialization.Tests/LaptopXmlDocument.cs
+++ XmlSerializationSample.XmlSerialization.Tests/LaptopXmlDocument.cs
@@ -2,7 +2,7 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
-namespace XmlSerializationSample.XmlSerialization
+namespace XmlSerializationSample.XmlSerialization.Tests
 {
   using System.Xml.Serialization;
 
