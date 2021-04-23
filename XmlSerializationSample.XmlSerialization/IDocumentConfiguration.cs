e9ad3efd3aa63e4ec5d04d27d55f9cf420aa440f
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2021-04-24 00:53:13 +0300

    doc comments

--- XmlSerializationSample.XmlSerialization/IDocumentConfiguration.cs
+++ XmlSerializationSample.XmlSerialization/IDocumentConfiguration.cs
@@ -6,8 +6,12 @@ namespace XmlSerializationSample.XmlSerialization
 {
   using System.Xml.Serialization;
 
+  /// <summary>Provides a simple API to configure an XML serialization of a type.</summary>
+  /// <typeparam name="TDocument">A type of a document.</typeparam>
   public interface IDocumentConfiguration<TDocument>
   {
+    /// <summary>Configures XML overrides.</summary>
+    /// <param name="overrides">An object that allows you to override property, field, and class attributes when you use the <see cref="System.Xml.Serialization.XmlSerializer"/> to serialize or deserialize an object.</param>
     public void Configure(XmlAttributeOverrides overrides);
   }
 }
